8d31bb9bb909a7293034a0d0d5f4dacb3a4be2c2
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-04-18 13:40:11 -0400

    Define start_new_game method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,19 +1,67 @@
 # frozen_string_literal: true
+require 'yaml'
+
+# A YAML serializer
+module YAMLSerializable
+  def to_yaml 
+    obj = {}
+    instance_variables.map do |var|
+      obj[var] = instance_variable_get(var)
+    end
+
+    YAML.dump obj
+  end
+
+  def from_yaml(string, permitted_classes)
+    YAML.load(string, permitted_classes: permitted_classes)
+  end
+end
 
 # This houses the components of the Hangman game
 module Hangman
+  include YAMLSerializable
+
+  def start
+    loop do
+      puts "Start new game (n) or load saved game (s)?"
+      begin
+        game_option = gets.chomp.downcase
+        raise unless game_option == 'n' || game_option == 's'
+      rescue StandardError
+        puts 'Invalid input! Try again...'
+      else
+        if game_option == 'n'
+          start_new_game
+        elsif game_option == 's'
+          game_saves = Dir.entries('game_saves').slice(2..-1)
+          loop do
+            puts "Copy and paste a game to load #{game_saves}"
+            begin
+              game_save = gets.chomp.downcase
+              raise unless game_saves.include?(game_save)
+            rescue StandardError
+              puts 'Invalid input! Try again...'
+            else
+              game = self.from_yaml(File.open("game_saves/#{game_save}"), [Hangman::Game])
+              game.play
+            end
+          end
+        end
+      end
+    end
+  end 
 
   # This houses the logics of the game
   class Game
-    def initialize
-      @secret_word = ""
-      @available_letters = ('a'..'z').to_a
-      @guessed_letters = []
-      @incorrect_guesses_remaining = 6
+    def initialize(game_id, secret_word, available_letters, guessed_letters, incorrect_guesses_remaining)
+      @game_id = game_id
+      @secret_word = secret_word
+      @available_letters = available_letters
+      @guessed_letters = guessed_letters
+      @incorrect_guesses_remaining = incorrect_guesses_remaining
     end
 
     def play
-      @secret_word = random_word('google-10000-english-no-swears.txt', 5, 12)
       p display_word(@secret_word)
       loop do
         if !display_word(@secret_word).include?('_')
@@ -31,19 +79,32 @@ module Hangman
           puts "#{@incorrect_guesses_remaining} incorrect #{@incorrect_guesses_remaining > 1 ? "guesses" : "guess"} remaining"
         end
         p display_word(@secret_word)
+        loop do
+          puts 'Quit and save game? (y/n)'
+          begin
+            answer = gets.chomp.downcase
+            raise unless answer == 'y' || answer == 'n'
+          rescue StandardError
+            puts "Invalid input! Enter 'y' to quit and save or 'n' to continue..."
+          else
+            if answer == 'y'
+              Dir.mkdir('game_saves') unless Dir.exist?('game_saves')
+              filename = "game_saves/#{@game_id}.yaml"
+              File.open(filename, 'w') do |file|
+                file.puts self.to_yaml
+              end
+              exit
+            else
+              break
+            end
+          end
+        end
       end
     end
 
     private
 
-    def random_word(filename, min, max)
-      dictionary = File.readlines(filename)
-      filtered = dictionary.map{ |word| word.chomp }.select{ |word| word.length >= min && word.length <= max }
-      last_index = filtered.length - 1
-      filtered[rand(0..last_index)]
-    end
-
-    def guess_letter 
+    def guess_letter
       loop do
         puts 'Guess a letter'
         begin
@@ -66,8 +127,22 @@ module Hangman
       display
     end
   end
+
+  private
+
+  def random_word(filename, min, max)
+    dictionary = File.readlines(filename)
+    filtered = dictionary.map{ |word| word.chomp }.select{ |word| word.length >= min && word.length <= max }
+    last_index = filtered.length - 1
+    filtered[rand(0..last_index)]
+  end
+
+  def start_new_game
+    secret_word = random_word('google-10000-english-no-swears.txt', 5, 12)
+    Game.new(Time.now.to_s.gsub(' -0400', '').gsub(' ', '-'), secret_word, ('a'..'z').to_a, [], 6).play
+  end
 end
 
 include Hangman
 
-Game.new.play
+start
